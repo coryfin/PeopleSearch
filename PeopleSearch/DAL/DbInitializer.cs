26e90d2ae38a676566e5d6c41a8c9b9209c19eb2
Cory Finlinson <coryfin13@gmail.com>
2016-12-29 03:45:47 -0800

    Added person photos

--- PeopleSearch/DAL/DbInitializer.cs
+++ PeopleSearch/DAL/DbInitializer.cs
@@ -13,22 +13,22 @@ namespace PeopleSearch.DAL
         {
             var people = new List<Person>
             {
-            new Person{FirstName="Carson", LastName="Alexander", BirthDate=DateTime.Parse("2005-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Meredith", LastName="Alonso", BirthDate=DateTime.Parse("2002-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Arturo", LastName="Anand", BirthDate=DateTime.Parse("2003-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Gytis", LastName="Barzdukas", BirthDate=DateTime.Parse("2002-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Yan", LastName="Li", BirthDate=DateTime.Parse("2002-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Peggy", LastName="Justice", BirthDate=DateTime.Parse("2001-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
+            new Person{FirstName="Carson", LastName="Alexander", BirthDate=DateTime.Parse("1991-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Meredith", LastName="Alonso", BirthDate=DateTime.Parse("1989-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Arturo", LastName="Anand", BirthDate=DateTime.Parse("1994-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Gytis", LastName="Barzdukas", BirthDate=DateTime.Parse("1985-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Yan", LastName="Li", BirthDate=DateTime.Parse("1986-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Peggy", LastName="Justice", BirthDate=DateTime.Parse("1990-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
             new Person{FirstName="Laura", LastName="Norman", BirthDate=DateTime.Parse("2003-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"},
-            new Person{FirstName="Nino", LastName="Olivetto", BirthDate=DateTime.Parse("2005-09-01"), Interests = new List<Interest>(),
-                Street="123 Elm Street", City="New York", State="New York", ZipCode="55555"}
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"},
+            new Person{FirstName="Nino", LastName="Olivetto", BirthDate=DateTime.Parse("1982-09-01"), Interests = new List<Interest>(),
+                Street="123 Elm Street", City="New York", State="NY", ZipCode="55555"}
             };
 
             var interests = new List<Interest>
